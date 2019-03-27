52fab76ce90806686bc665ada17dcb0d89005adb
Charlie-Hill <charliegeorgehill0@gmail.com>
2019-03-27 10:31:40 +0000

    Added bootstrap

--- resources/views/exams/exams_view.blade.php
+++ resources/views/exams/exams_view.blade.php
@@ -4,26 +4,32 @@
 
 @section('content')
 	
-	<h2>Exam: {{$exam->exam_name}}</h2>
-	<h4>{{$exam->exam_description}}</h4>
-	<a href="{{route('exams index')}}"><span class="fas fa-arrow-left"></span> Back</a>
+	<div class="row">
+		<div class="col-lg-12">
+		<h2>Exam: {{$exam->exam_name}}</h2>
+		<h4>{{$exam->exam_description}}</h4>
+		<a href="{{route('exams index')}}"><span class="fas fa-arrow-left"></span> Back</a>
+		</div>
+	</div>
 
 	<hr>
 
-	<div id="questions">
-		@if(count($exam->questions))
-		<h6>Questions:</h6>
-			<ul>
-				@foreach($exam->questions as $question)
-					<li>
-						<a href="{{route('quiz view question', $question->id)}}">{{$question->quiz_question}}</a>
-					</li>
-				@endforeach
-			</ul>
-		@endif
+	<div class="row">
+		<div id="questions">
+			@if(count($exam->questions))
+			<h6>Questions:</h6>
+				<ul>
+					@foreach($exam->questions as $question)
+						<li>
+							<a href="{{route('quiz view question', $question->id)}}">{{$question->quiz_question}}</a>
+						</li>
+					@endforeach
+				</ul>
+			@endif
+		</div>
 	</div>
 
-	<button type="button" id="addQuestionBtn">Add Question <i class="fas fa-plus"></i></button>
+	<button type="button" class="btn btn-primary" id="addQuestionBtn">Add Question <i class="fas fa-plus"></i></button>
 
 @endsection
 
@@ -36,8 +42,8 @@
 			$(document).on('click', '#addQuestionBtn', function () {
 				$(this).hide();
 				$('#questions').append('\
-					<input type="text" name="quiz_question" id="quizQuestion" placeholder="Enter Question.." />\
-					<button type="button" id="submitAddQuestionBtn">Submit</button>\
+					<input type="text" class="form-control" name="quiz_question" id="quizQuestion" placeholder="Enter Question.." autocomplete="off" />\
+					<button type="button" class="btn btn-success" id="submitAddQuestionBtn">Submit</button>\
 					');
 			});
 
--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -5,6 +5,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Quiz - @yield('title')</title>
 
+	<link rel="stylesheet" href="{{asset('assets/css/custom_strap.min.css')}}">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<style>
 		.no-border {
@@ -16,9 +17,12 @@
 </head>
 <body>
 
-	@yield('content')
+	<div class="container">
+		@yield('content')
+	</div>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="{{asset('assets/js/jquery-331.min.js')}}"></script>
+	<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
 	@yield('scripts')
 
 </body>
